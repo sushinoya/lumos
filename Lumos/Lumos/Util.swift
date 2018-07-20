77cbd10ff3dc6f516ee53de4644b761c61ec4b4e
Suyash Shekhar <suyashshekhar@gmail.com>
2018-07-21 00:19:34 +0800

    Add valueForType

--- Lumos/Lumos/Util.swift
+++ Lumos/Lumos/Util.swift
@@ -8,6 +8,25 @@
 
 import Foundation
 
+class Util {
+    static func valueForType(type: String) -> String {
+        switch type {
+        case "c" : return "Int8"
+        case "s" : return "Int16"
+        case "i" : return "Int32"
+        case "q" : return "Int"
+        case "S" : return "UInt16"
+        case "I" : return "UInt32"
+        case "Q" : return "UInt"
+        case "B" : return "Bool"
+        case "d" : return "Double"
+        case "f" : return "Float"
+        case "{" : return "Decimal"
+        default: return type
+        }
+    }
+}
+
 public extension String {
     func toPointer(completion: @escaping (UnsafePointer<Int8>) -> ()) {
         self.withCString { (pointer) -> () in
