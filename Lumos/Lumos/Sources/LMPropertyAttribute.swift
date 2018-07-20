6026567a04cb490e9197f83bc45ff7bab00f8803
Suyash Shekhar <suyashshekhar@gmail.com>
2018-07-21 00:21:02 +0800

    Add attribute description

--- Lumos/Lumos/Sources/LMPropertyAttribute.swift
+++ Lumos/Lumos/Sources/LMPropertyAttribute.swift
@@ -9,13 +9,36 @@
 import Foundation
 
 public struct LMPropertyAttribute {
+    
     public let name: String
-    public let value: String // Empty string in most cases
+    public let value: String? // Empty string in most cases
     public let rawStruct: objc_property_attribute_t
     
     init(rawStruct: objc_property_attribute_t) {
-        self.name = String(cString: rawStruct.name)
-        self.value = String(cString: rawStruct.value)
+        self.name = LMPropertyAttribute.nameDescription(for: rawStruct.name)
         self.rawStruct = rawStruct
+        let value = String(cString: rawStruct.value)
+        
+        guard !value.isEmpty else {
+            self.value = nil
+            return
+        }
+        self.value = value
+    }
+    
+    private static func nameDescription(for argumentType: UnsafePointer<Int8>) -> String {
+        let stringRepresentation = String(cString: argumentType)
+        switch stringRepresentation {
+        case "R": return "readOnly"
+        case "N": return "nonAtomic"
+        case "D": return "dynamic"
+        case "W": return "weak"
+        case "C": return "copy"
+        case "&": return "retain"
+        case "G": return "getterSelector"
+        case "S": return "selector"
+        case "T": return "typeEncoding"
+        case "V": return "variableName"
+        default: return stringRepresentation }
     }
 }
