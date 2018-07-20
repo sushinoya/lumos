1e82473dedc877615afeb3b9100de4c1b563e6ac
Suyash Shekhar <suyashshekhar@gmail.com>
2018-07-21 00:15:58 +0800

    Incorporate Runtime helper methods

---
+++ Lumos/Lumos/Sources/RuntimeQueries.swift
@@ -0,0 +1,8 @@
+//
+//  RuntimeQueries.swift
+//  Lumos
+//
+//  Created by Suyash Shekhar on 20/7/18.
+//
+
+import Foundation
---
+++ Lumos/Lumos/Sources/Swizzler.swift
@@ -0,0 +1,8 @@
+//
+//  Swizzler.swift
+//  Lumos
+//
+//  Created by Suyash Shekhar on 20/7/18.
+//
+
+import Foundation
