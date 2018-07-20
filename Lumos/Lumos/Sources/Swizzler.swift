55d3b8e07ba105ce31fb017466a4c02e0382bd31
Suyash Shekhar <suyashshekhar@gmail.com>
2018-07-21 00:21:37 +0800

    Add general swizzle

--- Lumos/Lumos/Sources/Swizzler.swift
+++ Lumos/Lumos/Sources/Swizzler.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+extension Lumos {
+    
+    // Objective-C Methods used directly instead of LM wrapper structs to avoid overhead
+    public static func swizzle(originalClass: AnyClass, originalSelector: Selector, swizzledClass: AnyClass, swizzledSelector: Selector) {
+        guard let originalMethod = class_getInstanceMethod(originalClass, originalSelector),
+            let swizzledMethod = class_getInstanceMethod(swizzledClass, swizzledSelector) else {
+                return
+        }
+        
+        let didAddMethod = class_addMethod(originalClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+        
+        if didAddMethod {
+            class_replaceMethod(originalClass, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    }
+}
