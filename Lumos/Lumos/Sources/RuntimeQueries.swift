62bf55f187377d9a6ba8952add47cafdd887f0d6
Suyash Shekhar <suyashshekhar@gmail.com>
2018-07-21 00:23:08 +0800

    Add getAllClasses

--- Lumos/Lumos/Sources/RuntimeQueries.swift
+++ Lumos/Lumos/Sources/RuntimeQueries.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+extension Lumos {
+    static func getAllClasses() -> [AnyClass] {
+        let expectedClassCount = objc_getClassList(nil, 0)
+        let allClasses = UnsafeMutablePointer<AnyClass?>.allocate(capacity: Int(expectedClassCount))
+        
+        let autoreleasingAllClasses = AutoreleasingUnsafeMutablePointer<AnyClass>(allClasses)
+        let actualClassCount: Int32 = objc_getClassList(autoreleasingAllClasses, expectedClassCount)
+        
+        var classes = [AnyClass]()
+        for i in 0 ..< actualClassCount {
+            if let currentClass: AnyClass = allClasses[Int(i)] {
+                classes.append(currentClass)
+            }
+        }
+        
+        allClasses.deallocate()
+        return classes
+    }
+}
